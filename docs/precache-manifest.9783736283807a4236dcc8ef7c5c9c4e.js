0d4223d98d95b388dead8e3fe00190499b213e68
Guido Garcia <ggarcia@cmnsa.local>
2020-09-08 20:22:14 -0300

    Agregado el Docs

---
+++ docs/precache-manifest.9783736283807a4236dcc8ef7c5c9c4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab93bca952b66daf09131892f2183e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90583fe286f25c2ae902",
+    "url": "/static/css/main.f3ba4153.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90583fe286f25c2ae902",
+    "url": "/static/js/main.b5b8d759.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
